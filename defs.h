ecb7146d338caa0cd4c862f31bab3ca94f3c656d
Rosa Rosales <rosamilib@gmail.com>
2024-04-26 18:12:42 -0400

    File creation
    I created all the files that will be used in the development of our system.

---
+++ defs.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Este archivo debe contener las declaraciones de variables y funciones
+
+// ---------------------------------------------------------------------
+
+// Declaracion de funciones:
+
+
+
+// Declaracion de variables:
