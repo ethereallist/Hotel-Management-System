04dd7ef8c85bc8427fbdd06bf01404114aa40222
Rosa Rosales <rosamilib@gmail.com>
2024-04-26 20:02:48 -0400

    Structure creation
    I created multiple structures for the Hotel and Reservations.

--- defs.c
+++ defs.c
@@ -1,1 +1,24 @@
 #include "defs.h"
+
+struct Hotel
+{
+    int habitaciones[4][5];
+};
+
+struct Reserva
+{
+    char Nombre[50];
+    char Apellido[50];
+    int Numero_Id;
+    int Personas_Totales;
+    int Fecha_Entrada[5][7]; //formato de calendario 5 filas 7 columnas
+    int Fecha_Salida[5][7]; //formato de calendario 5 filas 7 columnas
+    char Tipo_Habitacion[20];
+    float Precio_Total;
+};
+
+
+// Validación de la disponibilidad de habitaciones según la categoría y fecha seleccionada:
+
+
+
