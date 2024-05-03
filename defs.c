db7eaa5614205083f9c765294aba703c088f52eb
Edwer Soret <edwer2soret@gmail.com>
2024-05-03 12:38:51 -0400

    Update dates validation

--- defs.c
+++ defs.c
@@ -154,48 +154,103 @@ void OpcionModificarReserva()
 
 
 
-//_____________________FUNCIONES DE VALIDACION______________
+//_____________________FUNCIONES DE FECHA______________
+
 int Validar_Fecha(date F)
 {
     // VOY TIRANDO ERROR POR CADA FECHA INVALIDA
 
     //Año invalido
-    //Acotacion: No voy a poner año invalido, PERO: para años muy anteriores, la fecha va a ser incorrecta, especialmente del 1583 para atras, pero esto puede incluso variar segun el país
-    
+    if (F.aa < 1800) return 0; //No se ponga a inventar
+	
     // Mes invalido
-	if ( F.mm < 1 || F.mm > 12) return 0; 
+	else if ( F.mm < 1 || F.mm > 12) return -1; 
 	
 	//Dia invalido
-    else if (F.dd < 1 || F.dd > 31) return 0; 
+    else if (F.dd < 1 || F.dd > 31) return -2; 
     
     // Excluir los dias 31 de los meses abril, junio, septiembre y noviembre.
     else if (F.mm == 4 || F.mm == 6 || F.mm == 9 || F.mm == 11) 
     {
-        if (F.dd > 30) return 0; //Mes de 30 dias
+        if (F.dd > 30) return -3; //Mes de 30 dias
     }
     
     //Excluir el los dias de febrero dependiendo de si es bisiesto o no
-    if ((F.aa % 4 == 0 && F.aa % 100 != 0) || (F.aa % 400 == 0)) //PARA VER SI SÍ ES BISIESTO
+    else if (F.mm == 2 && esBisiesto(F) == 1 ) //PARA VER SI SÍ ES BISIESTO
     {
-        // Si estoy en el mes de febrero
-        if (F.mm == 2 && !(F.dd >= 1) || !(F.dd <= 29))
+        if (F.dd > 29)
         {
             // Los dias deben estar (como es bisiesto) entre 1 y 29
-            return 0; // El año es bisiesto
+            return -4; // El año es bisiesto
         }
         
+        
     }
-    else if (F.mm == 2 && (F.dd > 28)) 
+    else if (F.dd > 28) 
     {
             // Los dias deben estar (como es bisiesto) entre 1 y 29
-            return 0;
+            return -5;
     }
     
     
     
-    //Si pasó TODAS LAS PRUEBAS, la fecha el valida:
+    //Si paso TODAS LAS PRUEBAS, la fecha el valida:
     return 1;
 }
+
+
+
+int esBisiesto(date F)
+{
+    // Verificar las condiciones para determinar si es bisiesto
+
+    if ((F.aa % 4 == 0 && F.aa % 100 != 0) || (F.aa % 400 == 0)) // PARA VER SI SÍ ES BISIESTO
+    {
+        F.Extra.Es_Bisiesto = 1;
+        return 1;
+    }
+    else
+    {
+        F.Extra.Es_Bisiesto = 0;
+        return 0;
+    }
+}
+
+
+int convertidor_a_dias_del_anio(date F)
+{
+    // Cada indice es un mes del anio y la cantidad de dias que equivalen en cada tipo de anio
+
+    int Dias_Del_Anio_Normal[12] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+    
+    // Dias de cada mes en un anio bisiesto
+    int Dias_Del_Anio_Bisiesto[12] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335};
+    int Dias_Totales_Estadia = 0, Indice_Mes = F.mm;
+    int Dias_Del_Anio;
+    
+    if (Validar_Fecha(F) == 1)
+    {
+        if(esBisiesto(F) == 1)
+        {
+            // Sumo los dias que ingreso el usuario + los dias que representa el mes en el anio
+            Dias_Del_Anio = F.dd + Dias_Del_Anio_Bisiesto[Indice_Mes-1];
+        }
+        else
+        {
+            Dias_Del_Anio = F.dd + Dias_Del_Anio_Normal[Indice_Mes-1];
+        }
+        
+        return Dias_Del_Anio;
+    }
+    else 
+    {
+        return 0;
+    }
+    
+}
+
+
+
 
 
 void Asignar_Categorias(T_Hotel Categorias)
@@ -434,24 +489,20 @@ void scan_Habitacion_Id(re * PtrReserva)
 
 void scan_Fecha_Entrada(re * PtrReserva)
 {
-    int Fecha_Es_Valida = 0;
     do
     {
 		printf("Introduzca la fecha de entrada (DD/MM/AAAA): ");
         scanf("%i/%i/%i", &(*PtrReserva).Entrada.dd, &(*PtrReserva).Entrada.mm, &(*PtrReserva).Entrada.aa);
-        Fecha_Es_Valida = Validar_Fecha((*PtrReserva).Entrada);
-    } while (Fecha_Es_Valida == 0);
+	} while ( Validar_Fecha((*PtrReserva).Entrada) != 1);
 }
 
 void scan_Fecha_Salida(re * PtrReserva)
 {
-	int Fecha_Es_Valida = 0;
     do
     {
 		printf("Introduzca la fecha de salida (DD/MM/AAAA): ");
         scanf("%i/%i/%i", &(*PtrReserva).Salida.dd, &(*PtrReserva).Salida.mm, &(*PtrReserva).Salida.aa);
-        Fecha_Es_Valida = Validar_Fecha((*PtrReserva).Salida);
-    } while (Fecha_Es_Valida == 0);
+    } while ( Validar_Fecha((*PtrReserva).Salida) != 1);
 }
 
 
